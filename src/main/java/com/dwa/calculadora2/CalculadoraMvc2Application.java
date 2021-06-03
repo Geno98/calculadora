03174ba0c65ea494f85185db6e08220278c98b07
Beba <Beba@Geno.DomTec.local>
2021-06-03 16:04:58 -0500

    calculadora

---
+++ src/main/java/com/dwa/calculadora2/CalculadoraMvc2Application.java
@@ -0,0 +1,13 @@
+package com.dwa.calculadora2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraMvc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraMvc2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/dwa/calculadora2/CalculadoraMvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwa.calculadora2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraMvc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
